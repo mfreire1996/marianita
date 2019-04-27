a3d51091b565837b7ea97f7e38fe720bd6124d8d
mfreire1996 <mfreire1996@gmail.com>
2019-04-27 02:13:51 -0400

    more work

---
+++ main.js
@@ -0,0 +1,41 @@
+TweenMax.from(".mom", 1.6, {
+  delay: 0.1,
+  opacity: 0,
+  y: 20, 
+  ease: Expo.easeInOut   
+});
+
+TweenMax.from(".intro", 1.6, {
+    delay: 0.4,
+    opacity: 0,
+    y: 20, 
+    ease: Expo.easeInOut   
+  });
+
+TweenMax.from(".intro-2", 1.6, {
+    delay: 0.5,
+    opacity: 0,
+    y: 20, 
+    ease: Expo.easeInOut   
+  });
+
+  TweenMax.from(".fa-facebook-square", 1.6, {
+    delay: 1.0,
+    opacity: 0,
+    y: 20, 
+    ease: Expo.easeInOut   
+  });
+
+  TweenMax.from(".fa-envelope", 1.6, {
+    delay: 1.1,
+    opacity: 0,
+    y: 20, 
+    ease: Expo.easeInOut   
+  });
+
+  TweenMax.from(".fa-volume-control-phone", 1.6, {
+    delay: 1.2,
+    opacity: 0,
+    y: 20, 
+    ease: Expo.easeInOut   
+  });
